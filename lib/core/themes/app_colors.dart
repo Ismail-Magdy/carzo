655271781ae35df076e4ec4b624bc2b42dab9a35
Ismail Magdy <ismailmagdy920@gmail.com>
2026-03-26 21:05:34 +0200

    feat: Refactor app configuration and structure for development and production flavors

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color mainBackgroundColor = Color(0xffEEEEEE);
-  static const Color primaryColor = Color(0xFF4A628A);
+  // static const Color primaryColor = Color(0xFF4A628A);
+  static const Color primaryColor = Color(0xff08431D);
   static const Color textColor = Color(0xff304563);
   static const Color secondaryTextColor = Color(0xff304563);
   static const Color greyColor = Color(0xffD7D7D7);
---
+++ lib/main_development.dart
@@ -0,0 +1,21 @@
+import 'package:carzo/carzo_app.dart';
+import 'package:carzo/core/di/dependency_injection.dart';
+import 'package:carzo/core/routes/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //
+  await SystemChrome.setPreferredOrientations([.portraitUp]);
+  //
+  await initGetIt();
+  //
+  await ScreenUtil.ensureScreenSize();
+  //
+  runApp(CarzoApp(appRouter: AppRouter()));
+}
+// flutter run --flavor development -t lib/main_development.dart
+
+// flutter run --flavor production -t lib/main_development.dart
